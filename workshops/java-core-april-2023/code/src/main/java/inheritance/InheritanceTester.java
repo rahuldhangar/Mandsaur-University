4937a15c0e01dd5c06d3be7d6315e60e33af063b
Rahul Dhangar <rahuldhangar@gmail.com>
2023-04-26 12:06:31 +0530

    Inheritance and Constructors related Example

---
+++ workshops/java-core-april-2023/code/src/main/java/inheritance/InheritanceTester.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class InheritanceTester {
+    public static void main(String[] args) {
+        Person p1 = new Person("Rahuld");
+        Employee e1 = new Employee();
+
+    }
+}
